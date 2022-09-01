6091412a0feb48108dbb87bd9135d8aaab524061
Alex <alex_bar2310@mail.ru>
2022-09-01 15:29:00 +0500

    task 52

---
+++ task52/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] matrix = new int[,]{{1,7,9,6},{6,8,4,3},{8,1,2,3}};
+Console.Write("Среднее арифметическое элементов в каждом столбце: ");
+
+for(int i = 0; i < matrix.GetLength(1); i++)
+{
+    double sum = 0;
+    for(int j = 0; j < matrix.GetLength(0); j++)
+    {
+        sum = sum + matrix[j,i];
+    }
+    double aver = sum/matrix.GetLength(0);
+    if(i > 0)
+    {
+        Console.Write("; ");
+    }
+    Console.Write($"{aver:f1}");
+}
+Console.WriteLine(".");
